232454e3166a72a249f7937438856781bcf10ce1
Kristy Kelly <kristy@kristykelly.com>
2025-03-07 21:07:18 -0500

    Initial Commit

---
+++ NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by Kristy Kelly on 3/7/25.
+//
+
+import Testing
+@testable import NewsApp
+
+struct NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
