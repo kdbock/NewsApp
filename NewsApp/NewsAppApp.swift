ee958b41c2522b26b3b4e61c2b1d8836dda7cb15
Kristy Kelly <kristy@kristykelly.com>
2025-03-07 21:07:18 -0500

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Kristy Kelly on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
